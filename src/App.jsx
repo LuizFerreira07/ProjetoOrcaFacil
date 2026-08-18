ba0791686a5bd1d5a82d6d7585d75e57618246c6
Antônio <grantonio250@gmail.com>
2026-08-18 16:34:04 -0300

    footer

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <main>
+
+        </main>
+        <footer className="cabeçalho">
+      <div className="envolvedorIcones">
+        <svg className="icones"
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+            >
+      <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+    <polyline points="9 22 9 12 15 12 15 22" />
+  </svg>
+
+  <svg className="icones"
+   xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M17 1l4 4-4 4" />
+      <path d="M3 11V9a4 4 0 0 1 4-4h14" />
+      <path d="M7 23l-4-4 4-4" />
+      <path d="M21 13v2a4 4 0 0 1-4 4H3" />
+    </svg>
+
+  <svg className="icones"
+  xmlns="http://www.w3.org/2000/svg" 
+  width="24" 
+  height="24" 
+  viewBox="0 0 24 24" 
+  fill="none" 
+  stroke="currentColor" 
+  strokeWidth="2" 
+  strokeLinecap="round" 
+  strokeLinejoin="round"
+>
+  <line x1="18" y1="20" x2="18" y2="10" />
+  <line x1="12" y1="20" x2="12" y2="4" />
+  <line x1="6" y1="20" x2="6" y2="14" />
+</svg>
+
+      </div>
+          </footer>
+  </>
+  )
+}
+
+export default App
